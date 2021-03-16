ca82ae1831500fa20e173f04cd5e2ef6b614fa6b
younghoon.choi <c_yh0327@naver.com>
2021-03-16 17:52:56 +0900

    [chore] 변수명 수정

--- src/modules/dateNow.js
+++ src/modules/dateNow.js
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 
-const dateNow = function (plusHour) {
-    const hour = plusHour || 0;
+const dateNow = function (incrementTime) {
+    const hour = incrementTime || 0;
     return dayjs(new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' }))
         .add(hour, 'h')
         .format('YYYY-MM-DD HH:mm:ss');
